d2d389993e9f4035addfa51304993104043560f2
Felipe Prata Lima <felipepratalima@gmail.com>
2019-02-26 02:34:30 -0300

    Function to assign metaomics origin for centrifuge data added;

---
+++ R/assignCentrifugeProfileAsMetatranscriptomics.R
@@ -0,0 +1,38 @@
+require(taxdumpr)
+require(magrittr)
+require(stringr)
+require(dplyr)
+
+#'
+#'
+#' @param centrifugeProfile
+#' @param centrifugeClassificationsSummaries
+assignCentrifugeProfileAsMetatranscriptomics <- function(centrifugeProfile = NULL, centrifugeClassificationsSummaries = NULL) {
+  if (centrifugeProfile %>% is.null) {
+    stop("The centrifugeProfile parameter should not be null")
+  }
+
+  if (centrifugeProfile %>% is.na) {
+    stop("The centrifugeProfile parameter should not be NA")
+  }
+
+  if (centrifugeClassificationsSummaries %>% is.null) {
+    stop("The centrifugeClassificationsSummaries parameter should not be null")
+  }
+
+  if (centrifugeClassificationsSummaries %>% is.na) {
+    stop("The centrifugeClassificationsSummaries parameter should not be NA")
+  }
+
+  ## Merging
+  gungaProfile <- centrifugeProfile %>% merge(centrifugeClassificationsSummaries, by = c("rank", "standardId"))
+
+  ## Update names
+  namesIndexes <- (names(gungaProfile) %in% c("rank", "standardId") == FALSE)
+  names(gungaProfile)[namesIndexes] <- names(gungaProfile)[namesIndexes] %>% paste0("ForMt")
+
+  ## Assign meta-omics origin
+  gungaProfile$centrifugeMt <- TRUE
+
+  return(gungaProfile)
+}
