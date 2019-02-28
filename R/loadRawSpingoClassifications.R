2d8af8c7339f99d7d83856a5e771cdc4a56a3a1f
Felipe Prata Lima <felipepratalima@gmail.com>
2019-02-28 15:29:34 -0300

    Functions to load raw spingo classifications modified (columns names);

--- R/loadRawSpingoClassifications.R
+++ R/loadRawSpingoClassifications.R
@@ -5,6 +5,6 @@ loadRawSpingoClassifications <- function(spingoClassificationsLocation = NULL) {
   # spingoClassifications <- read.delim(spingoClassificationsLocation, header = F, sep = "\t")
   # colnames(spingoClassifications) <- c("readId", "score", "clostridiumGroup", "clostridiumGroupScore", "genusName", "genusScore", "speciesName", "speciesScore")
   spingoClassifications <- readr::read_tsv(spingoClassificationsLocation,
-                                           col_names = c("readId", "score", "clostridiumGroup", "clostridiumGroupScore", "genusName", "genusScore", "speciesName", "speciesScore"))
+                                           col_names = c("spingoReadId", "spingoScore", "spingoClostridiumGroup", "spingoClostridiumGroupScore", "spingoGenusName", "spingoGenusScore", "spingoSpeciesName", "spingoSpeciesScore"))
   return(spingoClassifications)
 }
