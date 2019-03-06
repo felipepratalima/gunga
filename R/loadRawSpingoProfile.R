ade96773f6dbe6199b567fea1feab7fc2bdce237
Felipe Prata Lima <felipepratalima@gmail.com>
2019-03-06 01:57:59 -0300

    Functions to load raw spingo profile modifications added;

--- R/loadRawSpingoProfile.R
+++ R/loadRawSpingoProfile.R
@@ -3,10 +3,9 @@ require(readr)
 #'
 #'
 #' @param spingoProfileLocation
-#' @param taxonomyRank
-loadRawSpingoProfile <- function(spingoProfileLocation = NULL, taxonomyRank = "species") {
+loadRawSpingoProfile <- function(spingoProfileLocation = NULL) {
   spingoProfile <- readr::read_tsv(spingoProfileLocation, col_names = c("spingoTaxonomyName", "spingoRelativeAbundance"))
-  spingoProfile$rank <- taxonomyRank
+  # spingoProfile$rank <- taxonomyRank
   # colnames(spingoProfile) <- c("speciesName", "relativeAbundance")
   return(spingoProfile)
 }
